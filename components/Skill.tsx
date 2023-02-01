ca1359c5d1bc01d8348dd141086b846553f78cdb
Ahmad Arif <arifmassih6@gmail.com>
2023-02-01 13:21:24 -0500

    framer motion translation  not working for skills section on some mobile. Abandoning for now

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 export default function Skill({ directionLeft }: Props) {
   return (
-    <div className="group relative flex cursor-pointer">
+    <motion.div className="group relative flex cursor-pointer">
       <motion.img
-        initial={{ x: directionLeft ? -200 : 200 }}
-        whileInView={{ x: 0 }}
-        transition={{ duration: 1 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 2 }}
         viewport={{ once: true }}
         className="rounded-full border border-gray-500 object-cover w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
         src="./mememan.jpeg"
@@ -24,6 +24,6 @@ export default function Skill({ directionLeft }: Props) {
           </p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
