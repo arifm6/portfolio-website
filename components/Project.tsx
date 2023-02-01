9110833114a7c8e8464a982b93454168042b8d87
Ahmad Arif <arifmassih6@gmail.com>
2023-02-01 14:25:51 -0500

    updated animation on projects to include link to build and not just the image

--- components/Project.tsx
+++ components/Project.tsx
@@ -13,24 +13,21 @@ export default function Project({}: Props) {
       viewport={{ once: true }}
       className="w-screen flex-shrink-0 snap-center flex flex-col space-y-4 pb-4 items-center justify-center sm:p20 md:p-44"
     >
-      <a
+      <motion.a
+        initial={{ y: -150, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        viewport={{ once: true }}
         target="_blank"
         href="https://github.com"
         rel="noopener noreferrer"
         className="group flex items-center justify-center max-w-[80vw] "
       >
-        <motion.img
-          initial={{ y: -150, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          viewport={{ once: true }}
-          src="./mememan.jpeg"
-          alt=""
-        />
+        <motion.img src="./mememan.jpeg" alt="" />
         <h3 className=" absolute scale-0 group-hover:scale-100 p-4 border border-gray-300 rounded-full bg-gray-500 bg-opacity-30 text-3xl duration-200 text-center whitespace-nowrap ">
           Link To Project
         </h3>
-      </a>
+      </motion.a>
       <h4 className="text-xl sm:text-4xl font-semibold text-center">
         <span className="underline decoration-[#FFE55C]/50">
           Project 1 of 5:
