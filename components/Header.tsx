8f05d0eeb4761be243ef719c697a97021c5db37d
Ahmad Arif <arifmassih6@gmail.com>
2023-01-31 22:43:53 -0500

    added hero section and hover over state effect for header. Note this is not scalable or reusable long term

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,48 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
+  //note this is not a good way to do this but due to the low amount of items and time constraint, it is fine for now
+  const [whatIsHovered, setWhatIsHovered] = useState(-1);
+  const handleMouseEnter = (target: any) => {
+    setWhatIsHovered(target);
+  };
+  const handleMouseExit = () => {
+    setWhatIsHovered(-1);
+  };
   return (
     <header className="sticky top-0 p-3 sm:p-5 flex items-center justify-between mx-auto max-w-7xl text-white z-10">
       <motion.div
         initial={{ x: -500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
-        className="flex items-center"
+        className="flex items-center transition-colors duration-1000"
       >
         <SocialIcon
           url="https://www.linkedin.com/in/ahmadmarif/"
-          fgColor="gray"
+          fgColor={`${whatIsHovered === 0 ? "#F7AB0A" : "gray"}`}
           bgColor="transparent"
+          onMouseEnter={() => handleMouseEnter(0)}
+          onMouseLeave={() => handleMouseExit()}
         />
         <SocialIcon
           url="https://github.com/arifm6"
-          fgColor="gray"
+          fgColor={`${whatIsHovered === 1 ? "#F7AB0A" : "gray"}`}
           bgColor="transparent"
+          onMouseEnter={() => handleMouseEnter(1)}
+          onMouseLeave={() => handleMouseExit()}
         />
-        <SocialIcon url="" fgColor="gray" bgColor="transparent" />
+        <SocialIcon
+          url=""
+          fgColor={`${whatIsHovered === 2 ? "#F7AB0A" : "gray"}`}
+          bgColor="transparent"
+          onMouseEnter={() => handleMouseEnter(2)}
+          onMouseLeave={() => handleMouseExit()}
+        />
       </motion.div>
 
       <motion.div
@@ -32,11 +50,13 @@ export default function Header({}: Props) {
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
         className="flex flex-row items-center text-gray-300 cursor-pointer "
+        onMouseEnter={() => handleMouseEnter(3)}
+        onMouseLeave={() => handleMouseExit()}
       >
         <SocialIcon
           className="cursor-pointer"
           network="email"
-          fgColor="gray"
+          fgColor={`${whatIsHovered === 3 ? "#F7AB0A" : "gray"}`}
           bgColor="transparent"
         />
         <p className="hidden sm:inline-flex text-sm uppercase">Contact Me!</p>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,51 @@
 import React from "react";
-
+import BackgroundCircles from "./BackgroundCircles";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import Link from "next/link";
 type Props = {};
 
 export default function Hero({}: Props) {
-  return <div className="h-screen">Hero Start</div>;
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Ahmad Arif",
+      "NextJS-Web-Develepor",
+      "<ButEnjoysOtherStuffToo />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full w-32 h-32 mx-auto object-cover"
+        src="./mememan.jpeg"
+        alt=""
+      />
+
+      <div className="space-y-4 z-20 ">
+        <h2 className="tracking-[10px] text-sm uppercase text-gray-500 mt-4 sm:tracking-[15px]  ">
+          Software Engineer
+        </h2>
+        <h1 className="text-2xl md:text-4xl lg:text-6xl px-10 font-semibold h-12 w-full sm:h-24  2xl:h-1/3  ">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div>
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
