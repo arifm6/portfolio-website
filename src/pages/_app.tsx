3bec1f8f1e952e84aabbc937e6558d3e7572b796
Arifm6 <arifmassih6@gmail.com>
2023-10-03 10:15:46 -0400

    Implemented GA Package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script id="google-analytics">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
-      </Script>
-
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
       <Analytics />
     </>
